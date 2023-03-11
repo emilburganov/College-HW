7f71efd5b7fb142fe3b540d77446f675f3f69e65
Emil <98642406+emilburganov@users.noreply.github.com>
2023-03-11 20:47:21 +0300

    Add files via upload

---
+++ Algorithmization and programming/Practice №19/BankAccount.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice__19
+{
+    internal class BankAccount
+    {
+        private string Surname;
+        private string Name;
+        private string Patronymic;
+        private string accountNumber;
+        public string AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+            set
+            {
+                
+                while(value.Length != 20)
+                {
+                    Console.WriteLine("< 20");
+                    value = Console.ReadLine();
+                }
+                
+                accountNumber = value;
+            }
+        }
+        public double Balance;
+
+        public BankAccount(string surname, string name, string patronymic, string accountNumber, double balance)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public string Info
+        {
+            get
+            {
+                return $"Полное имя клиента - {FullName} \nНомер счёта - {this.AccountNumber} \nБаланс - {Balance}";
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $"{Surname} {Name[0]}.{Patronymic[0]}.";
+            }
+        }
+        public double TopUpAccount
+        {
+            set
+            {
+                if (value > 0) Balance += value;
+                else
+                {
+                    Console.WriteLine("\nСумма пополнения не может быть отрицательной!");
+                }
+            }
+        }
+        public double WithdrawMoneyFromAccount
+        {
+            set
+            {
+                if (value <= Balance && value >= 0) 
+                    Balance -= value;
+                else if (value < 0) 
+                    Console.WriteLine("\nСумма снятия не может быть отрицательной!");
+                else
+                {
+                    Console.WriteLine("\nСумма снятия не может быть больше остатка!");
+                }
+            }
+        }
+    }
+}
---
+++ Algorithmization and programming/Practice №19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice__19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Бурганов", "Эмиль", "Эдуардович", "1234567890412340", 0);
+            account.TopUpAccount = 1000;
+            account.TopUpAccount = 4000;
+            account.WithdrawMoneyFromAccount = -1000;
+
+
+            List<BankAccount> bankAccounts = new List<BankAccount>();
+            bankAccounts.Add(new BankAccount("Бурганов", "Эмиль", "Эдуардович", "12345678901234567890", 10));
+            bankAccounts.Add(new BankAccount("Осипов", "Эмиль", "Эдуардович", "12345678901234567890", 2000));
+            bankAccounts.Add(new BankAccount("Петров", "Эмиль", "Эдуардович", "12345678901234567890", 100));
+            bankAccounts.Add(new BankAccount("Алексеев", "Эмиль", "Эдуардович", "12345678901234567890", 5000));
+            bankAccounts.Add(new BankAccount("Климов", "Эмиль", "Эдуардович", "123456789041234567890", 90000));
+
+            Console.WriteLine("\nДо сортировки:");
+            foreach (BankAccount bankAccount in bankAccounts)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(bankAccount.Info);
+            }
+
+            bankAccounts.Sort((f, s) => string.Compare(f.FullName, s.FullName));
+
+            Console.WriteLine("\nПосле сортировки по алфавиту:");
+            foreach (BankAccount bankAccount in bankAccounts)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(bankAccount.FullName);
+            }
+
+            double min = 10;
+            double max = 1000;
+
+            Console.WriteLine("\nПосле сортировки по балансу:");
+            foreach (BankAccount bankAccount in bankAccounts)
+                if (bankAccount.Balance >= min && bankAccount.Balance <= max)
+                    Console.WriteLine($"У {bankAccount.FullName} баланс = {bankAccount.Balance}");
+
+            Console.ReadLine();
+        }
+    }
+}
